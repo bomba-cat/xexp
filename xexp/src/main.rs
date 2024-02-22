1a567730bbb3a7b8082de7235ed43b20bf948c9f
xk-rl <bledion.morina@hotmail.com>
2024-02-22 23:15:22 +0100

    button loop

--- xexp/src/main.rs
+++ xexp/src/main.rs
@@ -1,7 +1,18 @@
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow, Button, Grid};
+use gtk::{Application, ApplicationWindow, Button, Label, Grid};
+use std::process::Command;
 
 fn main() {
+    let mut pwd = if cfg!(target_os = "windows") {
+        Command::new("dir")
+    } else {
+        Command::new("ls")
+    };
+
+    pwd.status().expect("failed to execute command!");
+
+    println!();
+
     let app = Application::builder()
         .application_id("org.xexp.explorer")
         .build();
@@ -9,16 +20,31 @@ fn main() {
     app.connect_activate(|app| {
         let win = ApplicationWindow::builder()
             .application(app)
-            .default_width(320)
-            .default_height(200)
+            .default_width(720)
+            .default_height(480)
             .title("xexp explorer")
             .build();
-        
+ 
+        let button1 = Button::with_label("1");
+        button1.connect_clicked(|_| {
+            eprintln!("1");
+        });
+
+        let button2 = Button::with_label("2");
+        button2.connect_clicked(|_| {
+            eprintln!("2");
+        });
+
         let grid = Grid::new();
         grid.set_row_spacing(10);
         grid.set_column_spacing(10);
-        grid.attach(&Button::with_label("Button 1"), 0, 0, 1, 1);
-        grid.attach(&Button::with_label("Button 2"), 1, 0, 1, 1);
+        
+        for index in 0..10 {
+            grid.attach(&Label::new(Some("Click me!")), index, 0, 1, 1);
+            grid.attach(&Button::with_label("(me)"), index, 1, 1, 1);
+        }
+        
+        win.add(&grid);
 
         win.show_all();
     });
